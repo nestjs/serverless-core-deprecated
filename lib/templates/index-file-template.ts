fc68ad141c383359c5d9f40fc4e8ffc27b57c5eb
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-12 11:51:28 +0200

    fix() replace default template

--- lib/templates/index-file-template.ts
+++ lib/templates/index-file-template.ts
@@ -3,18 +3,13 @@ import { defaultEntryFilename } from '../constants';
 export const defaultEntryModulePath = `./${defaultEntryFilename}`;
 
 export const INDEX_FILE_TEMPLATE = (text: any, moduleName: string) => `
-import { Context, HttpRequest } from '@azure/functions';
-import { AzureHttpAdapter } from '@nestjs/azure-func-http';
 import { NestFactory } from '@nestjs/core';
 import { ${moduleName} } from '${defaultEntryModulePath}';
 
 export async function createApp() {
   const app = await NestFactory.create(${moduleName});
-  await app.init();
-  return app;
+  return app.listen(3000)
 }
 
-export default function(context: Context, req: HttpRequest): void {
-  AzureHttpAdapter.handle(createApp, context, req);
-}
+createApp()
 `;
